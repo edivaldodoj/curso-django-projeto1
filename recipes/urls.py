1f1e04760d45cc2c02cbd819f2440ad24a7f9420
edivaldodoj <edivaldodoj.dev@gmail.com>
2024-11-24 00:07:09 -0300

    testt

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .import views
 
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home),
-    path('receitas/<int:id>/', views.recipe),
+    path('', views.home, name='home'),
+    path('receitas/<int:id>/', views.recipe, name='recipe'),
 ]
