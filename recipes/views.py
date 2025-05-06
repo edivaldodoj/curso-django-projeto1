719c0c715be21ff8750c54a8619e63054a339a6b
Edivaldo Junior <edivaldodoj.dev@gmail.com>
2025-05-06 15:13:34 -0300

    Primeiro commit

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,4 @@
-from django.http import Http404
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from utils.recipes.factory import make_recipe
 from . models import Recipe
 
@@ -26,13 +25,26 @@ def category(request, category_id):
 
 
 def recipe(request, id):
+    # recipe = Recipe.objects.get(id=id)
     
-    recipe = Recipe.objects.get(id=id)
+    recipe = get_object_or_404(Recipe, id=id, is_published=True,)
     return render(request, 'recipes/pages/recipe-view.html', context={
         
         'recipe': recipe,
-        'is_detail_page': True
+        'is_detail_page': True,
     })
+    
+
+# def recipe(request, id):
+#     recipe = Recipe.objects.filter(
+#         pk=id,
+#         is_published=True,
+#     ).order_by(-id).first()
+    
+#     return render(request, 'recipes/pages/recipe-view.html', context={
+#         'recipe': recipe,
+#         'is_detail_page': True,
+#     })
     
 
 def teste(request):
