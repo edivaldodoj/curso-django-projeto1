c944be23b2245ed3f38a6106f003fd828fcc34d4
Edivaldo Junior <edivaldodoj.dev@gmail.com>
2025-05-28 22:55:30 -0300

    testando

--- recipes/views.py
+++ recipes/views.py
@@ -4,12 +4,14 @@ from . models import Recipe
 
 
 def home(request):
-    # recipes = Recipe.objects.all().order_by('-id')
-    recipes = get_list_or_404(
-        Recipe.objects.filter(
-            is_published=True,
-        ).order_by('-id')
-    )
+    
+    recipes = Recipe.objects.filter(is_published=True,).order_by('-id')
+    
+    # recipes = get_list_or_404(
+    #     Recipe.objects.filter(
+    #         is_published=True,
+    #     ).order_by('-id')
+    # )
     
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
