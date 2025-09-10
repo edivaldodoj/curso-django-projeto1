69875b9f78ee853135f1089ab089d39bcc7a2993
Edivaldo Junior <edivaldodoj.dev@gmail.com>
2025-09-10 01:51:01 -0300

    test

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
 
 class RecipeURLsTest(TestCase):
-    def test_the_pytest_is_ok(self):
-        ...
+    def test_recipe_home_url_is_correct(self):
+        ...   
