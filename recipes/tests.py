115f68514a7c6cdefe10eebc13be64949b94bc3c
Edivaldo Junior <edivaldodoj.dev@gmail.com>
2025-09-10 02:07:31 -0300

    teste

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
 class RecipeURLsTest(TestCase):
     def test_recipe_home_url_is_correct(self):
-        ...   
+        home_url = reverse('recipes:home')
+        self.assertEqual(home_url, '/')
+        
