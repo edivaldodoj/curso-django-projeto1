90639f9c33146972219c83fbda998372167bcb2c
Edivaldo Junior <edivaldodoj.dev@gmail.com>
2025-09-27 11:05:11 -0300

    t

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
 
 class RecipeURLsTest(TestCase):
     def test_recipe_home_url_is_correct(self):
